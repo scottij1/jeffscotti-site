846e4c33bb0cd7ca5b24fba781dac115b4094b64
Jeff Scotti <jeffscotti@gmail.com>
2025-03-02 13:57:03 -0800

    Built testimonials slider, added to home

---
+++ src/data/testimonials.ts
@@ -0,0 +1,115 @@
+export interface Testimonial {
+  id: number;
+  name: string;
+  company: string;
+  image: string;
+  text: string;
+  visible?: boolean;
+}
+
+export const testimonials: Testimonial[] = [
+  {
+    id: 1,
+    name: "Chelsea Coster",
+    company: "Product Director at Twilio",
+    image: "/testimonials/headshot_chelsea_coster.jpg",
+    text: "Jeff is an engaged team player and is very solution oriented, considering the broader context and application when grooming/planning feature work or his implementation approach. He is a hard worker and conscientious. He is scrappy and resourceful when tackling new domain. I appreciated his integrity and positive attitude and would leap to have him on my team again.",
+    visible: true
+  },
+  {
+    id: 2,
+    name: "Adam Conde",
+    company: "Sr. Director of Information Technology at Boundless Bio",
+    image: "/testimonials/headshot_adam_conde.jpg",
+    text: "I hired Jeff a little over a year ago with the hope of hiring a strong WordPress developer. What I got instead was an incredibley talented software engineer, a dedicated worker, a great person, and friend. He is a logical and methodical problem solver. He has the rare combination of a developer with legitimate design and UX skills. Which made a huge difference in the quality of our web projects. I am confident that he would bring these traits and skills to any employer and I hope that one day I can work with him again.",
+    visible: true
+  },
+  {
+    id: 3,
+    name: "Matthew DeFauw",
+    company: "Senior Software Product Manager at Guardant Health",
+    image: "/testimonials/headshot_matt_defauw.jpg",
+    text: "I have the pleasure of working with Jeff for the past 2 years. He is an ideal developer to have as a colleague. Easy to work with, speaking up when things could work better, while also being honest if a question is outside his skillset. However, he will also quickly learn, adapt, and figure things out. Besides having extremely strong and modern technical developer skills, he is also able to meet with business users directly to understand their requirements, see through the confusion or lack of details, and then create an efficient, well-designed, and scalable end-to-end solution which captures the heart of the business need.",
+    visible: true
+  },
+  {
+    id: 4,
+    name: "Greg Lamb",
+    company: "Principle, Architecture and Development at Launch Consulting Group",
+    image: "/testimonials/headshot_greg_lamb.jpg",
+    text: "I had the pleasure of working with Jeff while on contract at MediaAMP. As user interface ideas were discussed, I was impressed by Jeff's design experience and his ability to bring them to life quickly. With his strong JavaScript and CSS skills, Jeff would be a great asset to any team.",
+    visible: true
+  },
+  {
+    id: 5,
+    name: "Glyn Beaumont",
+    company: "Managing Director at Hemisphere",
+    image: "/testimonials/headshot_glyn_beaumont.jpg",
+    text: "I've had the privilege of working with Jeff on an extremely ambitious project , with tight deadlines and many demanding stakeholders. Our entire team agreed that Jeff was not only a very, very capable UX/UI designer, but his enthusiasm and unwavering dedication formed the cornerstone of our success. I'd work with Jeff again in a heartbeat, and hope to have the opportunity to do so.",
+    visible: true
+  },
+  {
+    id: 6,
+    name: "Scott Maxon",
+    company: "Senior Consultant at Seabourne",
+    image: "/testimonials/headshot_scott_maxon.jpg",
+    text: "Jeff has a rare combination of excellent coding skills and an eye for clean, user-friendly front end design. Jeff has built a strong sense of what works well in UX, and the products he develops show it. He's willing to learn, listens well, and picks up new paradigms quickly. This makes him a pleasure to work with.",
+    visible: true
+  },
+  {
+    id: 7,
+    name: "Alex Walker",
+    company: "Software Engineer at Benaroya Research Institute",
+    image: "/testimonials/headshot_alex_walker.jpg",
+    text: "Jeff has an excellent grasp of modern design and user experience development. His CSS and JavaScript skills are very strong and he is always eager to learn new things. He and I worked together for over a year on several different projects and he always delivered great work on what were often very aggressive timelines. He was also extremely flexible as we dealt with ever-changing requirements. I hope to work with him again",
+    visible: true
+  },
+  {
+    id: 8,
+    name: "Joan Davis",
+    company: "Senior Technology Analyst",
+    image: "/testimonials/headshot_joan_davis.jpg",
+    text: "Jeff is an extremely talented web designer and developer. I admire his dedication to his craft and passion for his work. Jeff and I worked on a web application that would be used by many individuals who were not particularly tech savvy. Jeff always kept the user experience at the forefront and designed simple, clean interfaces. Working with Jeff was a pleasure. He was willing to do any task needed to get the job done. I recommend Jeff without hesitation. ",
+    visible: true
+  },
+  {
+    id: 9,
+    name: "Stephan Reese",
+    company: "",
+    image: "/testimonials/headshot_stephan_reese.jpg",
+    text: "I've had the pleasure of seeing Jeff in action as a Project Manager (as a client to my company) and as Developer (as a member of my own team). As a manager, Jeff is incredibly hands-on and personally invested. It is clear that we cares a great deal about the quality of work that goes into his projects, and is not afraid to jump in the trenches with the development team to get things done. As a developer, Jeff is highly-skilled and driven. In times that I was swamped with multiple projects, I knew that I could task Jeff with ANYTHING and know that it he would get it done in the required time-frame at a very high level of quality. Having excelled in both these roles makes Jeff a highly skilled developer WITH incredible soft skills. I would never say no to working with him in any capacity.",
+    visible: false
+  },
+  {
+    id: 10,
+    name: "John Haslam",
+    company: "Founder, Executive Director at 50 | Northwest",
+    image: "/testimonials/headshot_john_haslam.jpg",
+    text: "In the two years I have worked with Jeff I have found him to be a dedicated, hard working and highly creative individual. Jeff's contributions to the success UWTV has had in the online space are too many to list. His most imprtant contribution, however, is developing a brand and look for uwtv.org that has provided a fantactic user experience and led to sustained growth in traffic and time on site. Jeff masterfully blends cutting edge design and UI/UX best practices with an ability to produce a large volume of work. Any organization wishing to improve their audience's web experience and increase traffic would be well served to consider adding Jeff as a member of their team.",
+    visible: true
+  },
+  {
+    id: 11,
+    name: "Phillippa Kassover",
+    company: "",
+    image: "/testimonials/headshot_phillippa_kassover.jpg",
+    text: "Jeff is an extraordinarily talented designer, whose background gives him a real edge in the field. His knowledge of social psychology makes him a very effective web developer, who understands his audiences and caters to their needs with compelling, creative and tasteful design. Jeff was a great partner and collaborator in all our marketing efforts, who came through with ideas and design that exceeded expectations on every occasion. His love and knowledge of sports took UWTV to new levels of design and marketing professionalism and engagement as we rolled out our first sports-related programming. Jeff is always focused on the user as he designs sites, making them simple and accessible for all audiences. Jeff was also organized, thorough, accountable and responsive in his relationship with me and other leaders at UWTV. He has since taken on a greater level of responsibility and has fulfilled all expectations. I would be thrilled to work with Jeff again and wish him the very best in his future career.",
+    visible: false
+  },
+  {
+    id: 12,
+    name: "Bob Zat",
+    company: "",
+    image: "/testimonials/headshot_bob_zat.jpg",
+    text: "I have had the pleasure of working with Jeff on numerous motion graphic design projects for UWTV. His designs are well though out based upon the needs and desires of the client, and works well in a collaborative environment to deliver a quality product that meets or exceeds the expectations of the client. He is intelligent, creative, a good listener, and thoughtful of others. It has been a pleasure working with Jeff and I hope we might work together again.",
+    visible: false
+  },
+  {
+    id: 13,
+    name: "Tim Polack",
+    company: "",
+    image: "/testimonials/headshot_tim_polack.jpg",
+    text: "I  hired Jeff to be the graphic designer for me as I began a small Web group in the College of Agriculture. His work in graphic design and in coding out sites using HTML/CSS was critical to the success of this group as he joined with a small group in serving a growing number of clients both inside and outside the college. His energetic and self-starter qualities separated Jeff from other workers I hired in my five years as Web group lead. He creatively tackled issues and effectively learned other technologies, such as flash, that helped raise the professionalism and user-centered nature of our sites and applications. I recommend Jeff for work in both the Graphic Design and Webmaster fields.",
+    visible: false
+  },
+];
