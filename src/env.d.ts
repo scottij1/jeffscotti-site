c32abbd81d17f8250fe5faad439b926fb944a86c
Jeff Scotti <jeffscotti@gmail.com>
2025-03-04 18:29:23 -0800

    Changed up animation on my name and job titles

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,25 @@
 /// <reference types="astro/client" />
 
+// Image files
+declare module "*.png" {
+  const src: string;
+  export default { src };
+}
+
+// PDF files
+declare module "*.pdf" {
+  const src: string;
+  export default src;
+}
+
+// DOCX files
 declare module "*.docx" {
+  const content: string;
+  export default content;
+}
+
+// DOCX files with URL query
+declare module "*.docx?url" {
   const content: string;
   export default content;
 }
