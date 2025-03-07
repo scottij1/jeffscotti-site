dd3b2e9793afeb3ec6d187999c1a1967d0407501
Jeff Scotti <jeffscotti@gmail.com>
2025-03-07 09:03:51 -0800

    Added GA4 tags

--- src/env.server.ts
+++ src/env.server.ts
@@ -9,6 +9,8 @@ export const ENV = {
   SMTP_USER: process.env.SMTP_USER || 'jeff@jeffscotti.net',
   SMTP_PASS: process.env.SMTP_PASS || '',
   CONTACT_EMAIL: process.env.CONTACT_EMAIL || 'jeff@jeffscotti.net',
+  GA_MEASUREMENT_ID: process.env.GA_MEASUREMENT_ID || 'G-PV1N962H4B',
+  GA_TAG_ID: process.env.GA_TAG_ID || 'GT-MBTZSXW',
 };
 
 export function validateEnv() {
@@ -19,6 +21,8 @@ export function validateEnv() {
   if (!ENV.SMTP_USER) missingVars.push('SMTP_USER');
   if (!ENV.SMTP_PASS) missingVars.push('SMTP_PASS');
   if (!ENV.CONTACT_EMAIL) missingVars.push('CONTACT_EMAIL');
+  if (!ENV.GA_MEASUREMENT_ID) missingVars.push('GA_MEASUREMENT_ID');
+  if (!ENV.GA_TAG_ID) missingVars.push('GA_TAG_ID');
   
   if (missingVars.length > 0) {
     return false;
