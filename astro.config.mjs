0154d0ae622acc4f7fc84e08b7bfc5d55addeed7
Jeff Scotti <jeffscotti@gmail.com>
2025-03-07 15:39:56 -0800

    Cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,20 +5,15 @@ export default defineConfig({
   // Use static site generation instead of server-side rendering
   output: 'static',
   
-  // Optional: Add other configuration options as needed
   site: 'https://jeffscotti.net',
   
-  // Build options
   build: {
     // Always inline small stylesheets to improve LCP
     inlineStylesheets: 'always',
   },
   
-  // Set the directory where your site will be built
-  // The default is './dist'
   outDir: './dist',
   
-  // Optional: Configure the server during development
   server: {
     port: 4321,
     host: true,
